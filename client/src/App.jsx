17b9b79572abb656f45a6c2449582b509605ae6b
mpriyanshi03 <mpriyanshi2003@gmail.com>
2025-09-11 01:10:20 +0530

    Fix App.jsx imports for deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
-import Login from './pages/login';
+import Login from './pages/Login';
 import ResetPassword from './pages/ResetPassword';
 import EmailVerify from './pages/EmailVerify';
 import { ToastContainer } from 'react-toastify';
